92f212ad2ed7d7e6797e228aed7660318fcdc4da
dterbah <dterbah@hevaweb.com>
2024-07-01 16:10:21 +0200

    feat: Add version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,28 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+var (
+	version     string
+	showVersion bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "gopm",
 	Short: "Gopm is a package manager for Go",
 	Long:  `Gopm is a CLI tool to manage Go project dependencies.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			fmt.Printf("Version : %s\n", version)
+			return
+		}
+	},
 }
 
 func Execute() {
@@ -31,4 +43,7 @@ func init() {
 		PadLevelText:           true,
 	})
 	logrus.SetLevel(logrus.InfoLevel)
+
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "Show version")
+	version = "1.0.3"
 }
